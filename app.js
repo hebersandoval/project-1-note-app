10cda83a25c782b61d81e5a5398140437e7c386c
Heber Sandoval <he3ers@gmail.com>
2022-04-18 12:41:21 -0400

    Can remove all elements of a certain type.

--- app.js
+++ app.js
@@ -3,4 +3,11 @@ console.log('Hello from Notes App!');
 // Query document for varies elements in page.
 const p = document.querySelector('p');
 
-p.remove();
+// p.remove();
+
+// Query all and remove
+const ps = document.querySelectorAll('p');
+
+ps.forEach(function (p) {
+    p.remove();
+});
