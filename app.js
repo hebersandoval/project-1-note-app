ac3bbe19c870e41bae4b080f687754ed0777fa4d
Heber Sandoval <he3ers@gmail.com>
2022-04-18 13:25:25 -0400

    Ability to add new element in the DOM.

--- app.js
+++ app.js
@@ -12,3 +12,11 @@ ps.forEach(function (p) {
     // Change content for each paragraph's value.
     p.textContent = '*********************';
 });
+
+// Add a new element to page
+// 1. Create a new element
+const newParagraph = document.createElement('p');
+// 2. Update content
+newParagraph.textContent = 'This is a new element from JS code.';
+// 3. Put it somewhere in the DOM
+document.querySelector('body').appendChild(newParagraph); // Will append 'newParagraph' to the end of the list of tag selected.
