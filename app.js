9ddebbdc2cc398c393960f02028b6d3d0a071d64
Heber Sandoval <he3ers@gmail.com>
2022-04-18 13:52:24 -0400

    Create a static notes object that contains some basic information for now.

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 console.log('Hello from Notes App!');
 
+// Series of notes hard-coded
+const notes = [
+    {
+        title: 'Learn JavaScript Types in a day.',
+        content: 'It would be nice to learn all you can about JavaScript types in a day and remember then.',
+    },
+    {
+        title: 'Plan a trip to the beach or cabin in the woods.',
+        content: 'If you forget this, you wont be able to go anywhere this summer',
+    },
+    {
+        title: 'Start working on your JavaScript project.',
+        content: 'What are you going to build or what idea is in your head?',
+    },
+];
+
 // Query document for varies elements in page.
 const p = document.querySelector('p');
 
